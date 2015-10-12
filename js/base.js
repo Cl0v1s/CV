2b4bd6f63e4357869ac1767f754e8cd5f02cfccc
Clovis Portron <clovis.portron@etu.u-bordeaux.fr>
2015-10-12 17:59:32 +0200

    djksqjd'

--- js/base.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-  //action lors du click sur le bouton commencer
-  $("#next").click(function(){panel.show();});
-  $(window).mousewheel(function(){panel.show();});
-  //action lors de la redimension de la fenetre
-  $(window).resize(function(){panel.redimension();});
-});
